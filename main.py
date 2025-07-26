6bc03ad4ba4aa71ea9ea1b457cf0f1f39e62d545
iamRUDRAKSH <rudrakshcharhate@gmail.com>
2025-07-26 16:30:55 +0530

    I hope this is right

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from starlette.middleware.sessions import SessionMiddleware
 from app.routes import auth
 from app.routes import onboard
 import os
-FastAPI.add_middleware(SessionMiddleware, secret_key=os.getenv("SESSION_SECRET", "dev_secret"))
+
 app = FastAPI(title="SkillIssue Backend API", version="1.0.0")
 
 origins = [
@@ -17,9 +17,12 @@ app.add_middleware(
     allow_methods=["*"],              # Allow all HTTP methods
     allow_headers=["*"],              # Allow all headers
 )
+
 app.add_middleware(SessionMiddleware, secret_key=os.getenv("SESSION_SECRET", "dev_secret"))
+
 app.include_router(auth.router, prefix="/auth", tags=["auth"])
 app.include_router(onboard.router, prefix="/onboard", tags=["onboard"])
+
 @app.get("/")
 def home():
     return {"message": "I hope this works "}
